4ac8095d1afc7c06f6dcd583ad9cb7cf1c1bfa57
Will Fahnestock <wfahnestock@access411.com>
2025-01-08 13:38:36 -0500

    Add common plugins and dependencies (kotlinx-serialization, compose navigation)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,18 +2,22 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
+    // TODO: Change namespace to your app's namespace
     namespace = "com.changeme.androidsinglemodulestarter"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
+        // TODO: Change applicationId to your app's package name
         applicationId = "com.changeme.androidsinglemodulestarter"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,11 +40,13 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
 dependencies {
 
+    /* Dependencies */
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,11 +55,17 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.kotlin.serialization)
+    implementation(libs.androidx.navigation)
+
+    /* Test Deps */
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    /* Debug Deps */
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
