d96e14a129762d8f80a7c6e498809819ed12c1d6
Puzzleous <58612540+wfahnestock@users.noreply.github.com>
2025-01-13 16:40:32 -0500

    Update build.gradle.kts - bump Java version to 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "22"
     }
     buildFeatures {
         compose = true
