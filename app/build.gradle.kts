6e130755619f4f897934e7df2bcdafd6e8db032d
Will Fahnestock <wfahnestock@access411.com>
2026-01-29 15:55:06 -0500

    Bump com.android.application from 8.13.2 to 9.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,12 +34,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_23
-        targetCompatibility = JavaVersion.VERSION_23
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_23)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
     buildFeatures {
